437a361f3844fdb224d575cb07255222d8375146
Leonid <shadrin@contractor.aveone.com>
2023-10-24 10:28:15 +0300

    db prep script

---
+++ utils/prepare-db.js
@@ -0,0 +1,50 @@
+const sqlite3 = require('sqlite3').verbose();
+
+
+let db = new sqlite3.Database('./db/employees.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
+
+db.exec(
+  `CREATE TABLE employees
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(50) NOT NULL,
+      surname VARCHAR(50) NOT NULL,
+      department_id INTEGER NOT NULL
+    );
+    
+    CREATE TABLE departments
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(50) NOT NULL
+    );
+
+    CREATE TABLE salaries
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      amount FLOAT NOT NULL,
+      date VARCHAR(50) NOT NULL,
+      employee_id INTEGER NOT NULL
+    );
+
+    CREATE TABLE donations
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      amount FLOAT NOT NULL,
+      date VARCHAR(50) NOT NULL,
+      employee_id INTEGER NOT NULL
+    );
+    `
+);
+
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
